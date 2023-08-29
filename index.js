fddb8ff4d2de6391d9d750b11771af687a90afd5
Rohovska Nika <nikarogovska@gmail.com>
2023-08-29 10:05:24 +0300

    waw-article

---
+++ index.js
@@ -0,0 +1,129 @@
+const path = require("path");
+const template = path.join(process.cwd(), "template");
+
+module.exports = async (waw) => {
+	waw.crud('article', {
+		get: {
+			query: (req) => {
+				if (req.user.is.admin) {
+					return {};
+				} else {
+					return {
+						moderators: req.user._id
+					};
+				}
+			}
+		},
+		update: {
+			query: (req) => {
+				if (req.user.is.admin) {
+					return {
+						_id: req.body._id
+					};
+				} else {
+					return {
+						moderators: req.user._id,
+						_id: req.body._id
+					};
+				}
+			}
+		},
+		delete: {
+			query: (req) => {
+				if (req.user.is.admin) {
+					return {
+						_id: req.body._id
+					};
+				} else {
+					return {
+						moderators: req.user._id,
+						_id: req.body._id
+					};
+				}
+			}
+		}
+	});
+
+	waw.build(template, "articles");
+	waw.serve_articles = {};
+	const articles = async (req, res) => {
+		if (typeof waw.serve_articles[req.get("host")] === "function") {
+			waw.serve_articles[req.get("host")](req, res);
+		} else {
+			const articles = await waw.articles(
+				req.params.tag_id ?
+					{ tag: req.params.tag_id } :
+					{}
+			);
+			res.send(
+				waw.render(
+					path.join(template, "dist", "articles.html"),
+					{
+						...waw.config,
+						article: articles.pop(),
+						articles,
+						description: 'You can explore various interesting articles about real estate, furniture, new designs, and more! We offer you a collection of engaging and informative materials related to the world of design and interior. You can discover the latest trends, find tips and ideas for decorating your living space, and learn about exciting projects and innovative solutions in the real estate industry.'
+					},
+					waw.translate(req)
+				)
+			);
+		}
+	}
+	waw.app.get("/articles", articles);
+	waw.app.get("/articles/:tag_id", articles);
+
+	waw.build(template, "article");
+	waw.serve_article = {};
+	waw.app.get("/article/:_id", async (req, res) => {
+		if (typeof waw.serve_article[req.get("host")] === "function") {
+			waw.serve_article[req.get("host")](req, res);
+		} else {
+			const article = await Article.findOne({
+				_id: req.params._id
+			});
+
+			const articles = await Article.find({
+				_id: {
+					$ne: req.params._id
+				}
+			}).limit(6);
+
+			res.send(
+				waw.render(path.join(template, "dist", "article.html"), {
+					...waw.config,
+					...{ article, articles },
+				})
+			);
+		}
+	});
+
+	waw.articles = async (query = {}, limit, count = false) => {
+		let exe = count ? waw.Article.countDocuments(query) : waw.Article.find(query);
+
+		if (limit) {
+			exe = exe.limit(limit);
+		}
+
+		return await exe;
+	};
+
+	waw.article = async (query) => {
+		return await waw.Article.findOne(query);
+	}
+
+	waw.on('article_create', (doc)=>{
+		if (doc.thumb) {
+			waw.save_file(doc.thumb);
+		}
+	});
+	waw.on('article_update', (doc) => {
+		if (doc.thumb) {
+			waw.save_file(doc.thumb);
+		}
+	});
+	waw.on('article_delete', (doc)=>{
+		if (doc.thumb) {
+			waw.delete_file(doc.thumb);
+		}
+	});
+};
