127f37b2a13c6e6238ee2eda4a716f33e594b24b
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-07 13:21:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,8 @@ module.exports = async (waw) => {
 					path.join(template, "dist", "articles.html"),
 					{
 						...waw.config,
-						articles,groups: waw.tag_groups('article'),
+						articles,
+						categories: waw.tag_groups('article'),
 						title: waw.config.articleTitle|| waw.config.title,
                                                 description: waw.config.articleDescription || waw.config.description,
                                                 image: waw.config.articleImage|| waw.config.image,
