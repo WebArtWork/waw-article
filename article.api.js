1efd9afe8b8d34dd5eef529baa16203db0080603
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-11-08 15:02:35 +0200

    Update article.api.js

--- article.api.js
+++ article.api.js
@@ -101,7 +101,7 @@ module.exports = async (waw) => {
 		if (typeof waw.serve_articles[req.get("host")] === "function") {
 			waw.serve_articles[req.get("host")](req, res);
 		} else {
-			const articles = await waw.articles(
+			const articles = await waw.Article.find(
 				req.params.tag_id ? { tag: req.params.tag_id } : {}
 			);
 			res.send(
