749777df195f8fa7ca6ee06e333bb0faef2346f5
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-11-08 17:31:26 +0200

    Update article.api.js

--- article.api.js
+++ article.api.js
@@ -136,7 +136,7 @@ module.exports = async (waw) => {
 				: { url: req.params._id }
 			);
 
-			const articles = await waw.Article.find({
+			const articles = await waw.articles({
 				_id: {
 					$ne: req.params._id,
 				},
