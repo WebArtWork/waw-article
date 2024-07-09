b606207815da7ebbdd6095773a9c1152a007605a
Narolskyi Denis <asotizianov@gmail.com>
2024-07-09 19:45:47 +0300

    latest ecommerce updates

--- article.api.js
+++ article.api.js
@@ -1,94 +1,87 @@
-const { log } = require("console");
-const path = require("path");
-
 module.exports = async (waw) => {
+	const ensure = waw.role("admin owner", async (req, res, next) => {
+		if (!req.user.is.admin) {
+			req.storeIds = (
+				await waw.Store.find({
+					moderators: req.user._id,
+				}).select("_id")
+			).map((s) => s.id);
+			req.tagsIds = (
+				await waw.Tag.find({
+					stores: req.storeIds,
+				}).select("_id")
+			).map((s) => s.id);
+		}
+		next();
+	});
 	waw.crud("article", {
-		get: [
-			{
-				name: "public",
-				ensure: waw.next,
-				query: () => {
-					return {};
-				},
-			},
-			{
-				name: "noveltys",
-				ensure: waw.next,
-				query: () => {
-					return {
-						isTemplate: true,
-					};
-				},
-			},
-			{
-				name: "links",
-				ensure: async (req, res, next) => {
-					if (req.user) {
-						req.crafts_ids = (
-							await waw.Article.find({
-								moderators: req.user._id,
-								isTemplate: true,
-							}).select("_id")
-						).map((p) => p.id);
-
-						next();
-					} else {
-						res.json([]);
-					}
-				},
-				query: (req) => {
-					return {
-						template: {
-							$in: req.noveltys_ids,
+		get: {
+			ensure,
+			query: (req) => {
+				return req.user.is.admin
+					? {}
+					: {
+						tags: {
+							$in: req.tagsIds,
 						},
 					};
-				},
 			},
-			{
-				name: "admin",
-				ensure: waw.role("admin"),
-				query: () => {
-					return {};
-				},
-			},
-
-		],
+		},
 		update: {
-			name: "admin",
-			ensure: waw.role("admin"),
+			ensure,
 			query: (req) => {
-				return { _id: req.body._id };
+				return req.user.is.admin
+					? {
+						_id: req.body._id,
+					}
+					: {
+						_id: req.body._id,
+						tags: {
+							$in: req.tagsIds,
+						},
+					};
 			},
 		},
 		delete: {
-			name: "admin",
-			ensure: waw.role("admin"),
+			ensure,
 			query: (req) => {
-				return { _id: req.body._id };
+				return req.user.is.admin
+					? {
+						_id: req.body._id,
+					}
+					: {
+						_id: req.body._id,
+						tags: {
+							$in: req.tagsIds,
+						},
+					};
 			},
 		},
 		fetch: {
-			ensure: waw.next,
+			ensure,
 			query: (req) => {
-				return {
-					_id: req.body._id,
-				};
+				return req.user.is.admin
+					? {
+						_id: req.body._id,
+					}
+					: {
+						_id: req.body._id,
+						tags: {
+							$in: req.tagsIds,
+						},
+					};
 			},
 		},
 		create: {
 			ensure: async (req, res, next) => {
 				if (req.body.name) {
-					req.body.url = req.body.name
-						.toLowerCase()
-						.replace(/[^a-z0-9]/g, "");
+					req.body.url = req.body.name.toLowerCase().replace(/[^a-z0-9]/g, "");
 				}
 				if (req.body.url) {
 					while (await waw.Article.count({ url: req.body.url })) {
 						const url = req.body.url.split("_");
 						req.body.url =
-							url[0] +
-							"_" +
-							(url.length > 1 ? Number(url[1]) + 1 : 1);
+							url[0] + "_" + (url.length > 1 ? Number(url[1]) + 1 : 1);
 					}
 				} else {
 					delete req.body.url;
@@ -146,12 +139,12 @@ module.exports = async (waw) => {
 					tags: {
 						$in: fillJson.tagsIds,
 					},
-					enabled: true
+					enabled: true,
 				}).lean();
 				for (const article of fillJson.allArticles) {
 					article.id = article._id.toString();
 					article._id = article._id.toString();
-					article.tags = (article.tags || []).map(t => t.toString());
+					article.tags = (article.tags || []).map((t) => t.toString());
 				}
 				fillJson.top_articles = fillJson.allArticles.filter((p) => {
 					return p.top;
@@ -164,8 +157,8 @@ module.exports = async (waw) => {
 	);
 	const tagsUpdate = async (tag) => {
 		setTimeout(() => {
-			for (const storeId of (tag.stores || [])) {
-				for (const reload of (reloads[storeId] || [])) {
+			for (const storeId of tag.stores || []) {
+				for (const reload of reloads[storeId] || []) {
 					reload();
 				}
 			}
@@ -187,133 +180,133 @@ module.exports = async (waw) => {
 	waw.on("article_delete", articlesUpdate);
 
 	/*
-	await waw.wait(2000);
-	if (waw.store_landing) {
-		waw.store_landing.articles = async (query) => {
-			return await waw.articles(query, 4);
-		};
-	}
+	  await waw.wait(2000);
+	  if (waw.store_landing) {
+		  waw.store_landing.articles = async (query) => {
+			  return await waw.articles(query, 4);
+		  };
+	  }
 
-	await waw.wait(1000);
+	  await waw.wait(1000);
 
-	waw.addJson('operatorArticles', async (operator, fillJson) => {
-		fillJson.articles = await waw.articles({
-			domain: operator.domain
-		});
+	  waw.addJson('operatorArticles', async (operator, fillJson) => {
+		  fillJson.articles = await waw.articles({
+			  domain: operator.domain
+		  });
 
-		fillJson.articlesByTag = [];
-		for (const article of fillJson.articles) {
-			if (!article.tag) continue;
-			const tagObj = fillJson.articlesByTag.find(c => c.id.toString() === article.tag.toString());
-			if (tagObj) {
-				tagObj.articles.push(article);
-			} else {
-				const tag = waw.getTag(article.tag);
+		  fillJson.articlesByTag = [];
+		  for (const article of fillJson.articles) {
+			  if (!article.tag) continue;
+			  const tagObj = fillJson.articlesByTag.find(c => c.id.toString() === article.tag.toString());
+			  if (tagObj) {
+				  tagObj.articles.push(article);
+			  } else {
+				  const tag = waw.getTag(article.tag);
 
-				fillJson.articlesByTag.push({
-					id: article.tag,
-					category: tag.category,
-					name: tag.name,
-					description: tag.description,
-					articles: [article]
-				})
-			}
-		}
+				  fillJson.articlesByTag.push({
+					  id: article.tag,
+					  category: tag.category,
+					  name: tag.name,
+					  description: tag.description,
+					  articles: [article]
+				  })
+			  }
+		  }
 
-		fillJson.articlesByCategory = [];
-		for (const byTag of fillJson.articlesByTag) {
-			const categoryObj = fillJson.articlesByCategory.find(c => c.id.toString() === byTag.category.toString());
-			if (categoryObj) {
-				categoryObj.tags.push(byTag);
+		  fillJson.articlesByCategory = [];
+		  for (const byTag of fillJson.articlesByTag) {
+			  const categoryObj = fillJson.articlesByCategory.find(c => c.id.toString() === byTag.category.toString());
+			  if (categoryObj) {
+				  categoryObj.tags.push(byTag);
 
-				for (const article of byTag.articles) {
-					if (!categoryObj.articles.find(s => s.id === article.id)) {
-						categoryObj.articles.push(article)
-					}
-				}
-			} else {
-				const category = waw.getCategory(byTag.category);
+				  for (const article of byTag.articles) {
+					  if (!categoryObj.articles.find(s => s.id === article.id)) {
+						  categoryObj.articles.push(article)
+					  }
+				  }
+			  } else {
+				  const category = waw.getCategory(byTag.category);
 
-				fillJson.articlesByCategory.push({
-					id: byTag.category,
-					name: category.name,
-					description: category.description,
-					articles: byTag.articles.slice(),
-					tags: [byTag]
-				})
-			}
-		}
-	}, 'Filling just all article documents');
+				  fillJson.articlesByCategory.push({
+					  id: byTag.category,
+					  name: category.name,
+					  description: category.description,
+					  articles: byTag.articles.slice(),
+					  tags: [byTag]
+				  })
+			  }
+		  }
+	  }, 'Filling just all article documents');
 
-	waw.addJson('operatorArticle', async (operator, fillJson, req) => {
-		fillJson.article = await waw.article({
-			domain: operator.domain,
-			_id: req.params._id
-		});
+	  waw.addJson('operatorArticle', async (operator, fillJson, req) => {
+		  fillJson.article = await waw.article({
+			  domain: operator.domain,
+			  _id: req.params._id
+		  });
 
-		fillJson.footer.article = fillJson.article;
-	}, 'Filling just all article documents');
-
-
-	waw.addJson('operatorTopArticles', async (operator, fillJson) => {
-		fillJson.topArticles = await waw.articles({
-			domain: operator.domain
-		}, 4);
-
-		fillJson.footer.topArticles = fillJson.topArticles;
-	}, 'Filling just all article documents');
+		  fillJson.footer.article = fillJson.article;
+	  }, 'Filling just all article documents');
 
 
+	  waw.addJson('operatorTopArticles', async (operator, fillJson) => {
+		  fillJson.topArticles = await waw.articles({
+			  domain: operator.domain
+		  }, 4);
 
-	waw.addJson('storeArticles', async (store, fillJson) => {
-		fillJson.articles = await waw.articles({
-			author: store.author
-		});
+		  fillJson.footer.topArticles = fillJson.topArticles;
+	  }, 'Filling just all article documents');
 
-		fillJson.articlesByTag = [];
-		for (const article of fillJson.articles) {
-			if (!article.tag) continue;
-			const tagObj = fillJson.articlesByTag.find(c => c.id.toString() === article.tag.toString());
-			if (tagObj) {
-				tagObj.articles.push(article);
-			} else {
-				const tag = waw.getTag(article.tag);
 
-				fillJson.articlesByTag.push({
-					id: article.tag,
-					category: tag.category,
-					name: tag.name,
-					description: tag.description,
-					articles: [article]
-				})
-			}
-		}
 
-		fillJson.articlesByCategory = [];
-		for (const byTag of fillJson.articlesByTag) {
-			const categoryObj = fillJson.articlesByCategory.find(c => c.id.toString() === byTag.category.toString());
-			if (categoryObj) {
-				categoryObj.tags.push(byTag);
+	  waw.addJson('storeArticles', async (store, fillJson) => {
+		  fillJson.articles = await waw.articles({
+			  author: store.author
+		  });
 
-				for (const article of byTag.articles) {
-					if (!categoryObj.articles.find(s => s.id === article.id)) {
-						categoryObj.articles.push(article)
-					}
-				}
-			} else {
-				const category = waw.getCategory(byTag.category);
+		  fillJson.articlesByTag = [];
+		  for (const article of fillJson.articles) {
+			  if (!article.tag) continue;
+			  const tagObj = fillJson.articlesByTag.find(c => c.id.toString() === article.tag.toString());
+			  if (tagObj) {
+				  tagObj.articles.push(article);
+			  } else {
+				  const tag = waw.getTag(article.tag);
 
-				fillJson.articlesByCategory.push({
-					id: byTag.category,
-					name: category.name,
-					description: category.description,
-					articles: byTag.articles.slice(),
-					tags: [byTag]
-				})
-			}
-		}
-	}, 'Filling just all article documents');
-*/
+				  fillJson.articlesByTag.push({
+					  id: article.tag,
+					  category: tag.category,
+					  name: tag.name,
+					  description: tag.description,
+					  articles: [article]
+				  })
+			  }
+		  }
+
+		  fillJson.articlesByCategory = [];
+		  for (const byTag of fillJson.articlesByTag) {
+			  const categoryObj = fillJson.articlesByCategory.find(c => c.id.toString() === byTag.category.toString());
+			  if (categoryObj) {
+				  categoryObj.tags.push(byTag);
+
+				  for (const article of byTag.articles) {
+					  if (!categoryObj.articles.find(s => s.id === article.id)) {
+						  categoryObj.articles.push(article)
+					  }
+				  }
+			  } else {
+				  const category = waw.getCategory(byTag.category);
+
+				  fillJson.articlesByCategory.push({
+					  id: byTag.category,
+					  name: category.name,
+					  description: category.description,
+					  articles: byTag.articles.slice(),
+					  tags: [byTag]
+				  })
+			  }
+		  }
+	  }, 'Filling just all article documents');
+  */
 	const fillTags = (tags, id, fillJson) => {
 		for (const tag of tags) {
 			if (tag._id === id) {
@@ -354,7 +347,7 @@ module.exports = async (waw) => {
 			}
 		}
 		return false;
-	}
+	};
 	waw.addJson(
 		"storeArticle",
 		async (store, fillJson, req) => {
@@ -377,13 +370,18 @@ module.exports = async (waw) => {
 		"Add tags and product to json"
 	);
 
-	waw.addJson('storeTopArticles', async (store, fillJson) => {
-		fillJson.topArticles = await waw.articles({
-			author: store.author,
-		}, 4);
+	waw.addJson(
+		"storeTopArticles",
+		async (store, fillJson) => {
+			fillJson.topArticles = await waw.articles(
+				{
+					author: store.author,
+				},
+				4
+			);
 
-
-		fillJson.footer.topArticles = fillJson.topArticles;
-	}, 'Filling just all article documents');
-
+			fillJson.footer.topArticles = fillJson.topArticles;
+		},
+		"Filling just all article documents"
+	);
 };
